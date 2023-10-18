320ea9c2d40bf89473c8f5b934038607fa5667eb
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-10-18 16:16:19 +0200

    .

--- src/DataFixtures/Jeu1.php
+++ src/DataFixtures/Jeu1.php
@@ -4,14 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Artist; // Import the Artist entity
 
 class Jeu1 extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $artist1 = new Artist();
 
+        $artist1->setName("Queens Of The Stone Age");
+        $artist1->setUrl("https://qotsa.com");
+        
+        $manager->persist($artist1);
         $manager->flush();
     }
 }
+
+

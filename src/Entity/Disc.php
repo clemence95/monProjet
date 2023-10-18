6172aae3632db05fb7a7b5e9ade9aa44f9e07d39
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-10-18 11:00:28 +0200

    .

--- src/Entity/Disc.php
+++ src/Entity/Disc.php
@@ -13,8 +13,83 @@ class Disc
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $picture = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $label = null;
+
+    #[ORM\ManyToOne(inversedBy: 'discs')]
+    private ?artist $artist = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): static
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getArtist(): ?artist
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(?artist $artist): static
+    {
+        $this->artist = $artist;
+
+        return $this;
     }
 }

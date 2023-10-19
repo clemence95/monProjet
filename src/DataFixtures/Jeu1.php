c22e9044bc24fd0489b9d04459908c8f41406b4c
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-10-19 13:57:34 +0200

    .

--- src/DataFixtures/Jeu1.php
+++ src/DataFixtures/Jeu1.php
@@ -4,20 +4,32 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Artist; // Import the Artist entity
+use App\Entity\Artist;
+use App\Entity\Disc;
 
 class Jeu1 extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        // $product = new Product();
+        // $manager->persist($product);
         $artist1 = new Artist();
 
-        $artist1->setname("Queens Of The Stone Age");
-        $artist1->seturl("https://qotsa.com");
-        
+        $artist1->setArtistName("Queens Of The Stone Age");
+        $artist1->setArtistUrl("https://qotsa.com/");
+
         $manager->persist($artist1);
+
+        $disc1 = new Disc();
+
+        $disc1->setTitle("Song for the Deaf");
+        $disc1->setPicture("https://en.wikipedia.org/wiki/Songs_for_the_Deaf#/media/File:Queens_of_the_Stone_Age_-_Songs_for_the_Deaf.png");
+        $disc1->setLabel("Interscope Records");
+
+        $manager->persist($disc1);
+
+        $artist1->addDisc($disc1);
+
         $manager->flush();
     }
 }
-
-

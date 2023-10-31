6c1789b9e0de277aabbe073abba34d2f1b466279
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-10-31 11:16:48 +0100

    .

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ArtistRepository;
 use App\Repository\DiscRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,12 +17,13 @@ class AccueilController extends AbstractController
 
     private $artistRepo;
     private $discRepo;
+    private $em;
 
-    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo)
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo, EntityManagerInterface $em)
     {
         $this->artistRepo = $artistRepo;
         $this->discRepo = $discRepo;
-
+        $this->em = $em;
     }
 
     #[Route('/accueil', name: 'app_accueil')]
@@ -30,13 +32,13 @@ class AccueilController extends AbstractController
 
         //on appelle la fonction `findAll()` du repository de la classe `Artist` afin de récupérer tous les artists de la base de données;
 
-        $artistes = $this->artistRepo->findAll();
-
+        $artistes = $this->artistRepo->getSomeArtists("Neil");
+        dd($artistes);
         return $this->render('accueil/accueil.html.twig', [
             'controller_name' => 'AccueilController',
             //on va envoyer à la vue notre variable qui stocke un tableau d'objets $artistes (c'est-à-dire tous les artistes trouvés dans la base de données)
-            'artistes' => $artistes
+            'artistes' => $artistes,
+
         ]);
     }
-
 }
